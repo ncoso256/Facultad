d15defd6180ff2c24c2e527bd53be239de05e8ac
Noé <121284724+ncoso256@users.noreply.github.com>
2024-05-09 20:52:41 -0300

    Create ej10.pas

---
+++ Pascal/Practica 2/Parte 2/ej10.pas
@@ -0,0 +1,56 @@
+program diez;
+
+function esvocal(c: char): boolean;
+begin
+	esvocal:= (c = 'a') or (c= 'e') or (c= 'i') or (c= 'o') or (c= 'u');
+end;
+
+procedure cumplea(var cumple: boolean);
+var
+	c: char;
+begin
+	writeln('Ingrese la secuencia A: ');
+	readln(c);
+	while (c <> '$') and (cumple) do begin
+		if (not esvocal(c)) then
+			cumple:= false
+		else 
+			readln(c);
+	end;
+end;
+function esconsonante(c: char): boolean;
+begin
+	esconsonante:= ((c >= 'n') and (c <= 'c') and (not esvocal(c)));
+end;
+procedure cumpleb(var cumple: boolean ; var long: integer);
+var
+	c: char;
+begin
+	writeln('Ingrese la secuencia B');
+	readln(c);
+	while (c <> '#') and (cumple) do begin
+		if (not esconsonante(c)) then
+			cumple:= false
+		else begin
+			long:= long + 1;
+			readln(c);
+		end;
+	end; 
+end;
+var
+	long: integer;
+	cumple: boolean;
+begin
+	cumple:= true;
+	cumplea(cumple);
+	if (cumple) then begin
+		long:= 0;
+		nocumplea(cumple, long);
+		if(cumple) then
+			writeln('Se cumple la secuencia')
+		else
+			writeln('No cumple con B');
+	end
+	else
+		writeln('No cumple con A');
+end.
