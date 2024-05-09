fc8ec7a200694042528c3ad8e2a563455634179b
Noé <121284724+ncoso256@users.noreply.github.com>
2024-05-09 20:42:55 -0300

    Create ej5.pas

---
+++ Pascal/Practica 2/ej5.pas
@@ -0,0 +1,19 @@
+program hola;
+procedure numero;
+var
+	num, par, parmax: integer;
+begin
+	parmax:= -998;
+	writeln('Escriba un numero: '); readln(num);
+	while(num <> -1) do begin
+		if ((num mod 2) = 0) then
+			par:= num;
+		if (par > parmax) then
+			parmax:= par;
+		writeln('Escriba un numero: '); readln(num);
+	end;
+	writeln('El numero par mas alto es: ', parmax);
+end;
+begin
+	numero;
+end.
