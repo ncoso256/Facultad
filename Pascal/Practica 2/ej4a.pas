61e6b4c7e1105f8a4821789c5cff8d7220673078
Noé <121284724+ncoso256@users.noreply.github.com>
2024-05-09 20:39:31 -0300

    Create ej4a.pas

---
+++ Pascal/Practica 2/ej4a.pas
@@ -0,0 +1,31 @@
+program alcance4a;
+var
+	a, b: integer;
+procedure uno;
+begin
+	a:= 1;
+	writeln(a);
+end;
+begin
+	a:= 1;
+	b:= 2;
+	uno; // imprime 1 
+	writeln(b, a); // imprime 21
+end.
+
+program alcance4b;
+procedure uno; // a no existe en el procedimiento
+begin
+	a:= 1; //error1
+	writeln(a); //error2
+end;
+var 
+	a,b: integer; // variables locales al programappal
+begin
+	a:= 1;
+	b:= 2;
+	uno;
+	writeln(b, a);
+end.
+
+// este programa tiene dos errores y el otro no 
