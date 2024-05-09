506175e7141063bf329952fa73cd03cd98336970
Noé <121284724+ncoso256@users.noreply.github.com>
2024-05-09 20:46:14 -0300

    Create ej2.pas

---
+++ Pascal/Practica 2/Parte 2/ej2.pas
@@ -0,0 +1,26 @@
+program ej4;
+procedure digparesimpares (num: integer; var par, impar: integer);
+var
+	dig: integer;
+begin
+	while (num <> 0) do begin
+		dig:= num mod 10;
+		if ((dig mod 2)= 0)then
+			par:= par + 1
+		else
+			impar:= impar + 1;
+		num:= num div 10;
+	end;
+end;
+var
+	dato, par, impar, total, cant: integer;
+begin
+	par:= 0;
+	impar:= 0;
+	repeat
+		readln(dato);
+		digparesimpares(dato,par,impar);
+	until(dato = 100);
+	writeln('pares: ',par, 'impares: ', impar);
+end.
+// a) imprime que hay 4 pares y 4 impares con 250, 35, 100
