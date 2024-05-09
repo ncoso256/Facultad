15b2400086d2c452657e6228424766a266be5ce8
Noé <121284724+ncoso256@users.noreply.github.com>
2024-05-09 20:37:56 -0300

    Create ej1.pas

---
+++ Pascal/Practica 2/ej1.pas
@@ -0,0 +1,16 @@
+program alcance1;
+var
+	a, b: integer;
+procedure uno;
+var 
+	b: integer;
+begin
+	b:= 3;
+	writeln(b);
+end;
+begin
+	a:= 1;
+	b:= 2;
+	uno; {escribe 3}
+	writeln(b,a); {escribe todo en uno b:= 2 y a:= 1 writeln(b,a) escribe 21}
+end.
