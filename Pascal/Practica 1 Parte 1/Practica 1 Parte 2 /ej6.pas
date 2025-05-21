e2af7ac6b498f2427c46de38da975c798b981366
Noé <121284724+ncoso256@users.noreply.github.com>
2025-05-21 10:58:47 -0300

    Update ej6.pas

--- Pascal/Practica 1 Parte 1/Practica 1 Parte 2 /ej6.pas
+++ Pascal/Practica 1 Parte 1/Practica 1 Parte 2 /ej6.pas
@@ -1,3 +1,12 @@
+{6. Realizar un programa que lea información de 200 productos de un supermercado. 
+De cada producto se lee código y precio (cada código es un número entre 1 y 200). 
+
+Informar en pantalla: 
+
+● Los códigos de los dos productos más baratos.
+
+● La cantidad de productos de más de 16 pesos con código par.}
+
 program ejercicio6;
 type
 	codigos = 1..200;
