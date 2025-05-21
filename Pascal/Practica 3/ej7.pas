7038a0f3412b639e639e3fa59a253e2441635174
Noé <121284724+ncoso256@users.noreply.github.com>
2025-05-21 11:49:23 -0300

    Update ej7.pas

--- Pascal/Practica 3/ej7.pas
+++ Pascal/Practica 3/ej7.pas
@@ -1,4 +1,15 @@
-```pascal
+{7. Realizar un programa que lea información de centros de investigación de Universidades Nacionales.
+De cada centro se lee su nombre abreviado (ej. LIDI, LIFIA, LINTI), la universidad a la que pertenece, la cantidad de investigadores y la cantidad de becarios que poseen.
+La información se lee de forma consecutiva por universidad y la lectura finaliza al leer un centro con 0 investigadores, que no debe procesarse.
+
+Informar:
+
+● Cantidad total de centros para cada universidad. 
+
+● Universidad con mayor cantidad de investigadores en sus centros.
+
+● Los dos centros con menor cantidad de becarios.}
+
 program siete;
 type
 	str20 = string[20];
