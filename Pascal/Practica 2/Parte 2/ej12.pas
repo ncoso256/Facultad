c10d47367b27e7d1ee1e818c5135d72e2ec2a942
Noé <121284724+ncoso256@users.noreply.github.com>
2025-05-21 11:31:35 -0300

    Update ej12.pas

--- Pascal/Practica 2/Parte 2/ej12.pas
+++ Pascal/Practica 2/Parte 2/ej12.pas
@@ -1,3 +1,25 @@
+{12. a. Realizar un módulo que calcule el rendimiento económico de una plantación de soja.
+El módulo debe recibir la cantidad de hectáreas (ha) sembradas, el tipo de zona de siembra (1: zona muy fértil, 2: zona estándar, 3: zona árida) y el precio en U$S de la tonelada de soja; y devolver el rendimiento económico esperado de dicha plantación. 
+Para calcular el rendimiento económico esperado debe considerar el siguiente rendimiento por tipo de zona:
+
+Tipo de zona-Rendimiento por ha
+1- 6 toneladas por ha
+2- 2,6 toneladas por ha
+3- 1,4 toneladas por ha
+
+b. ARBA desea procesar información obtenida de imágenes satelitales de campos sembrados con soja en la provincia de Buenos Aires.
+De cada campo se lee: localidad, cantidad de hectáreas sembradas y el tipo de zona (1, 2 ó 3).
+La lectura finaliza al leer un campo de 900 ha en la localidad ‘Saladillo’, que debe procesarse.
+El precio de la soja es de U$S320 por tn.
+
+Informar: 
+
+● La cantidad de campos de la localidad Tres de Febrero con rendimiento estimado superior a U$S 10.000.
+● La localidad del campo con mayor rendimiento económico esperado. 
+● La localidad del campo con menor rendimiento económico esperado. 
+● El rendimiento económico promedio.
+}
+
 program doce;
 type
 	tipoZonaFertil = 1..3;
