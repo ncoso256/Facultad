d1667ea170116349414d3f9caf411fd7a74f8120
Noé <121284724+ncoso256@users.noreply.github.com>
2025-05-21 11:18:32 -0300

    Update ej7ayb.pas

--- Pascal/Practica 2/Parte 2/ej7ayb.pas
+++ Pascal/Practica 2/Parte 2/ej7ayb.pas
@@ -1,3 +1,7 @@
+{7. a. Realizar un módulo que reciba como parámetro un número entero y retorne la cantidad de dígitos que posee y la suma de los mismos.
+b. Utilizando el módulo anterior, realizar un programa que lea una secuencia de números e imprima la cantidad total de dígitos leídos. 
+La lectura finaliza al leer un número cuyos dígitos suman exactamente 10, el cual debe procesarse.}
+
 program ej7ab;
 
 procedure descomponer(num: integer ; var cantdig, sumadig: integer);
