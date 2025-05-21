e8561a600623bff39c7cc293b3ae0d0defb00e5d
Noé <121284724+ncoso256@users.noreply.github.com>
2025-05-21 11:22:33 -0300

    Update ej10.pas

--- Pascal/Practica 2/Parte 2/ej10.pas
+++ Pascal/Practica 2/Parte 2/ej10.pas
@@ -1,3 +1,13 @@
+{10. Realizar un programa modularizado que lea una secuencia de caracteres y verifique si cumple con el patrón A$B#, donde:
+
+-A es una secuencia de sólo letras vocales 
+
+-B es unasecuencia de sólo caracteres alfabéticos sin letras vocales 
+
+- los caracteres $ y # seguro existen
+
+Nota: en caso de no cumplir, informar que parte del patrón no se cumplió.}
+
 program diez;
 
 function esvocal(c: char): boolean;
