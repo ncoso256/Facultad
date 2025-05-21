f84493bf07a5f934760aa554b65e8ae0e6049b36
Noé <121284724+ncoso256@users.noreply.github.com>
2025-05-21 10:55:01 -0300

    Update ej4.pas

--- Pascal/Practica 1 Parte 1/Practica 1 Parte 2 /ej4.pas
+++ Pascal/Practica 1 Parte 1/Practica 1 Parte 2 /ej4.pas
@@ -1,3 +1,11 @@
+{4. Realizar un programa que lea 1000 números enteros desde teclado. 
+Informar en pantalla cuáles son los dos números mínimos leídos. 
+
+a. Modifique el ejercicio anterior para que, en vez de leer 1000 números, la lectura finalice al leer el número 0, el cual debe procesarse. 
+
+b. Modifique el ejercicio anterior para que, en vez de leer 1000 números, la lectura finalice al leer el número 0, el cual no debe procesarse.
+}
+
 program p1p2_ej4;
 var
     num,min1,min2,i:integer;
