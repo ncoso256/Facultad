6ac17767ee44bffed70a6ca97a0c6bc906172f04
Noé <121284724+ncoso256@users.noreply.github.com>
2025-05-21 11:00:30 -0300

    Update ej7.pas

--- Pascal/Practica 1 Parte 1/Practica 1 Parte 2 /ej7.pas
+++ Pascal/Practica 1 Parte 1/Practica 1 Parte 2 /ej7.pas
@@ -1,3 +1,13 @@
+{7. Realizar un programa que lea desde teclado información de autos de carrera. 
+Para cada uno de los autos se lee el nombre del piloto y el tiempo total que le tomó finalizar la carrera. 
+En la carrera participaron 100 autos. 
+
+Informar en pantalla: 
+
+● Los nombres de los dos pilotos que finalizaron en los dos primeros puestos. 
+
+● Los nombres de los dos pilotos que finalizaron en los dos últimos puestos.}
+
 program ejercicio7;
 var
 	i:integer;
