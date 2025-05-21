b85f39ec6383a99ebdeb55029f846c6c10fd63b7
Noé <121284724+ncoso256@users.noreply.github.com>
2025-05-21 11:10:16 -0300

    Update ej1.pas

--- Pascal/Practica 2/Parte 2/ej1.pas
+++ Pascal/Practica 2/Parte 2/ej1.pas
@@ -1,3 +1,5 @@
+{1. Responda las preguntas en relación al siguiente programa:}
+
 program Ejercicio3;
 procedure suma(var num1: integer; var  num2:integer);
 begin
@@ -12,6 +14,10 @@ for i:= 1 to 5 do
 	suma(i,x);
 write(x); { imprimo las variable x }
 end.
+
+{a. ¿Qué imprime si se lee el valor 10 en la variable x ? 
+b. Qué imprime si se lee el valor 10 en la variable x y se cambia el encabezado del procedure por: procedure suma(num1: integer; num2:integer); 
+c. Qué sucede si se cambia el encabezado del procedure por: procedure suma(var num1: integer; var num2:integer);}
 
 // a) imprime si se lee en la variable x 10 imprime 25 
 // b) imprime 10 una copia de lo que leyo 
