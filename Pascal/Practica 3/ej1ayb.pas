5164324c4462aefaf52d1e85d2e9728fdabd1ae9
Noé <121284724+ncoso256@users.noreply.github.com>
2025-05-21 11:33:01 -0300

    Update ej1ayb.pas

--- Pascal/Practica 3/ej1ayb.pas
+++ Pascal/Practica 3/ej1ayb.pas
@@ -1,4 +1,10 @@
+{1. Dado el siguiente programa:
 
+a. Completar el programa principal para que lea información de alumnos (código, nombre, promedio) e informe la cantidad de alumnos leídos.
+La lectura finaliza cuando ingresa un alumno con código 0, que no debe procesarse. 
+Nota: utilizar el módulo leer. 
+b. Modificar al programa anterior para que, al finalizar la lectura de todos los alumnos, se informe también el nombre del alumno con mejor promedio.
+}
 program Registros;
 type
 	str20 = string[20];
