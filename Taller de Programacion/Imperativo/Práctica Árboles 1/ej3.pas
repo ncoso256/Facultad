73ad42deabe5f582410924e086b07476d7596e01
Noé <121284724+ncoso256@users.noreply.github.com>
2025-08-26 19:14:41 -0300

    Update ej3.pas

--- Taller de Programacion/Imperativo/Práctica Árboles 1/ej3.pas
+++ Taller de Programacion/Imperativo/Práctica Árboles 1/ej3.pas
@@ -13,25 +13,7 @@ legajo impar.
 c. Un módulo que reciba la estructura generada en a. e informe, para cada alumno, su legajo y
 su cantidad de finales aprobados (nota mayor o igual a 4).
 
-c. Un módulo que reciba la estructura generada en a. y un valor real. Este módulo debe
-retornar los legajos y promedios de los alumnos cuyo promedio supera el valor ingresado.
-}
-
-{3. Implementar un programa que contenga:
-
-a. Un módulo que lea información de los finales rendidos por los alumnos de la Facultad de
-Informática y los almacene en una estructura de datos. La información que se lee es legajo,
-código de materia, fecha y nota. La lectura de los alumnos finaliza con legajo 0. La estructura
-generada debe ser eficiente para la búsqueda por número de legajo y para cada alumno deben
-guardarse los finales que rindió en una lista.
-
-b. Un módulo que reciba la estructura generada en a. y retorne la cantidad de alumnos con
-legajo impar.
-
-c. Un módulo que reciba la estructura generada en a. e informe, para cada alumno, su legajo y
-su cantidad de finales aprobados (nota mayor o igual a 4).
-
-c. Un módulo que reciba la estructura generada en a. y un valor real. Este módulo debe
+d. Un módulo que reciba la estructura generada en a. y un valor real. Este módulo debe
 retornar los legajos y promedios de los alumnos cuyo promedio supera el valor ingresado.
 }
 
@@ -60,7 +42,7 @@ type
 
 procedure leeralumno(var a: alumno);
 begin
-    a.legajo:= random(100);
+    a.legajo:= random(10);
     writeln (' el legajo es: ',a.legajo); 
     if (a.legajo <> 0)then begin 
         a.codigo:= random(10)+ 1;
@@ -68,7 +50,9 @@ begin
         a.fecha:= random(31)+ 1;
         writeln (' la fecha es: ', a.fecha); 
         a.nota:= random(11);
-        writeln (' la nota es. ', a.nota); 
+        writeln (' la nota es: ', a.nota); 
+        writelnh ('-----------------------------------------------------------------');
+        writeln ('');
     end;
 end;
 
@@ -84,24 +68,28 @@ end;
 
 procedure cargararbol(var a: arbol; alu: alumno);
 begin 
-    if (a = nil) then begin
+    if (a = nil) then begin //Caso base: el árbol está vacío → creo un nuevo nodo para este legajo 
         new(a);
-        a^.dato.finales:= nil;
-        a^.dato.leg:= alu.legajo;
-        agregarAdelante(a^.dato.finales,alu);
-        a^.hi:= nil;
-        a^.hd:= nil;
+        a^.dato.finales := nil;
+        a^.dato.leg := alu.legajo;  // le paso el legajo de la lista al arbol 
+        agregarAdelante(a^.dato.finales, alu);// guardo este final en la lista del alumno dentro del arbol
+        a^.hi := nil;
+        a^.hd := nil;
     end
     else begin
-        if (alu.legajo < a^.dato.leg) then
+        if (alu.legajo = a^.dato.leg) then //Caso 1: el legajo ya está en el árbol,no creo un nodo nuevo, simplemente agrego este final a la lista de finales de ese alumno
+            agregarAdelante(a^.dato.finales, alu)
+
+        else if (alu.legajo < a^.dato.leg) then// Caso 2: el legajo es más chico que el actual, bajo por la izquierda 
             cargararbol(a^.hi, alu)
-        else
+
+        else// Caso 3: el legajo es más grande que el actual, bajo por la derecha 
             cargararbol(a^.hd, alu);
     end;
 end;
 
 
-procedure cargar (var a: arbol); 
+procedure cargar (var a: arbol); {probar con recursividad}
 var 
 	alu: alumno; 
 begin 
@@ -112,10 +100,11 @@ begin
 	end; 
 end; 
 
+//b. Un módulo que reciba la estructura generada en a. y retorne la cantidad de alumnos conlegajo impar.
 procedure incisoGG(l: lista ; var cantimpares: integer);
 begin
     if (l <> nil)then begin
-        if (l^.dato.legajo mod 2 = 1) then 
+        if (l^.dato.legajo mod 2 <> 0) then 
                 cantimpares:= cantimpares + 1;
         incisoGG(l^.sig, cantimpares);
     end;
@@ -131,9 +120,68 @@ begin
     end; 
 end; 
 
+//c. Un módulo que reciba la estructura generada en a. e informe, para cada alumno, su legajo y
+//su cantidad de finales aprobados (nota mayor o igual a 4).
+
+procedure incisofinales (l: lista; var cantaprobados: integer);
+begin 
+	if (l<> nil) then begin // recorro la lista
+		if (l^.dato.nota >= 4) then 
+			cantaprobados:= cantaprobados + 1; // si la nota es mayor o igualque 4 sumo uno al contador
+		incisofinales (l^.sig, cantaprobados); // paso al siguiente nodo
+	end; 
+end; 
+
+procedure incisoc (a: arbol);
+var 
+	cantaprobados: integer; // variable para contar la cantidad de aprobados 
+begin 
+	if (a<> nil) then begin
+		incisoc (a^.hi);
+		cantaprobados:= 0; // inicializo en 0 para cuando paso a la siguiente hoja (osea cambia el legajo), se resetea el contador de finales
+		incisofinales (a^.dato.finales, cantaprobados); // le paso la lista y la variable contadora
+		writeln ('el alumno con legajo: ', a^.dato.leg , ' tiene la cantidad de final/es aprobados de : ', cantaprobados); //imprimo
+		incisoc (a^.hd);
+	end; 
+end; 
+
+procedure calcularPromedio(l: lista; valor: real; legajo: integer);
+var
+    cantidad, notas: integer;
+    promedio: real;
+begin
+cantidad := 0;
+notas := 0;
+while (l <> nil) do begin// recorro toda la lista de finales del alumno
+    cantidad := cantidad + 1;
+    notas := notas + l^.dato.nota;
+    l := l^.sig;
+end;
+  if (cantidad > 0) then begin // pregunto si el alumno rindio finales
+    promedio := notas / cantidad; // saco el promedio e informo lo que pide el inciso 
+    if (promedio > valor) then begin
+        writeln('El alumno con legajo ', legajo, 
+            'tiene un promedio mayor al de ', valor:0:2, ' con un total de: ', promedio:0:2);
+    end;
+end;
+end;
+
+//d. Un módulo que reciba la estructura generada en a. y un valor real. Este módulo debe
+//retornar los legajos y promedios de los alumnos cuyo promedio supera el valor ingresado.
+
+procedure incisod(a: arbol; valor: real);
+begin
+if (a <> nil) then begin
+    incisod(a^.hi, valor); // bajo a las hojas izquierdas
+    calcularPromedio(a^.dato.finales, valor, a^.dato.leg);// llamo al proceso y le paso, la lista, el valor que ingrese y el legajo actual del alumno 
+    incisod(a^.hd, valor);// bajo a la hojas derechas
+end;
+end;
+
 var
     a: arbol;
     cantimpares: integer; 
+    valor: real;
 begin
     randomize;
     a:= nil;
@@ -141,4 +189,8 @@ begin
 	cargar (a);
     incisob (a, cantimpares); 
     writeln (' la cantidad de alumnos con legajo impar es de: ', cantimpares);
+    incisoc(a);
+    writeln (' ingrese un valor real para el ultimo inciso:'); 
+    readln (valor); 
+    incisod(a, valor); 
 end.
