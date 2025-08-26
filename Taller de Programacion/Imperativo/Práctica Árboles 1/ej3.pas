64a28ee8fa2e3229971a374949ecd3b18a6e3621
Noé <121284724+ncoso256@users.noreply.github.com>
2025-08-25 23:24:04 -0300

    Update ej3.pas

--- Taller de Programacion/Imperativo/Práctica Árboles 1/ej3.pas
+++ Taller de Programacion/Imperativo/Práctica Árboles 1/ej3.pas
@@ -1,3 +1,22 @@
+
+{3. Implementar un programa que contenga:
+
+a. Un módulo que lea información de los finales rendidos por los alumnos de la Facultad de
+Informática y los almacene en una estructura de datos. La información que se lee es legajo,
+código de materia, fecha y nota. La lectura de los alumnos finaliza con legajo 0. La estructura
+generada debe ser eficiente para la búsqueda por número de legajo y para cada alumno deben
+guardarse los finales que rindió en una lista.
+
+b. Un módulo que reciba la estructura generada en a. y retorne la cantidad de alumnos con
+legajo impar.
+
+c. Un módulo que reciba la estructura generada en a. e informe, para cada alumno, su legajo y
+su cantidad de finales aprobados (nota mayor o igual a 4).
+
+c. Un módulo que reciba la estructura generada en a. y un valor real. Este módulo debe
+retornar los legajos y promedios de los alumnos cuyo promedio supera el valor ingresado.
+}
+
 {3. Implementar un programa que contenga:
 
 a. Un módulo que lea información de los finales rendidos por los alumnos de la Facultad de
@@ -26,23 +45,34 @@ type
         dato: alumno;
         sig: lista;
     end;
+    
+    registroarbol = record
+        leg: integer; 
+        finales: lista; 
+    end; 
+
     arbol = ^nodoarbol;
     nodoarbol = record
-        dato: alumno;
+        dato: registroarbol;
         hi: arbol;
         hd: arbol;
     end;
 
 procedure leeralumno(var a: alumno);
 begin
-    a.legajo:= random(100) +1;
+    a.legajo:= random(100);
+    writeln (' el legajo es: ',a.legajo); 
     if (a.legajo <> 0)then begin 
         a.codigo:= random(10)+ 1;
+        writeln (' el codigo es: ', a.codigo);
         a.fecha:= random(31)+ 1;
+        writeln (' la fecha es: ', a.fecha); 
         a.nota:= random(11);
+        writeln (' la nota es. ', a.nota); 
     end;
 end;
-procedure agregarAdelante (var l: lista; a: alumno);
+
+procedure agregarAdelante (var l: lista; a:alumno);
 var
     nuevo:lista;
 begin
@@ -52,55 +82,63 @@ begin
     l:= nuevo;
 end;
 
-procedure cargarlista(var l: lista);
-var
-    a: alumno;
-begin
-    leeralumno(a);
-    while (a.legajo <> 0) do begin
-        agregarAdelante(l,a);
-        leeralumno(a);
-    end;
-end;
-
-procedure cargararbol(var a: arbol);
-var
-    l: lista;
+procedure cargararbol(var a: arbol; alu: alumno);
 begin 
-    a:= nil;
     if (a = nil) then begin
         new(a);
-        a^.dato:= l^.dato;
+        a^.dato.finales:= nil;
+        a^.dato.leg:= alu.legajo;
+        agregarAdelante(a^.dato.finales,alu);
         a^.hi:= nil;
         a^.hd:= nil;
     end
     else begin
-        if (l^.dato.legajo < a^.dato.legajo) then
-            cargararbol(a^.hi)
+        if (alu.legajo < a^.dato.leg) then
+            cargararbol(a^.hi, alu)
         else
-            cargararbol(a^.hd);
+            cargararbol(a^.hd, alu);
     end;
 end;
+
+
+procedure cargar (var a: arbol); 
+var 
+	alu: alumno; 
+begin 
+	leeralumno(alu); 
+	while (alu.legajo <> 0) do begin
+		cargararbol (a, alu); 
+		leeralumno(alu); 
+	end; 
+end; 
+
+procedure incisoGG(l: lista ; var cantimpares: integer);
+begin
+    if (l <> nil)then begin
+        if (l^.dato.legajo mod 2 = 1) then 
+                cantimpares:= cantimpares + 1;
+        incisoGG(l^.sig, cantimpares);
+    end;
+end;
+
 
 procedure incisob (a: arbol; var cantimpares: integer);
 begin 
     if (a<>nil) then begin 
-        if (a^.dato.legajo mod 2 <>0) then 
-            cantimpares:= cantimpares + 1; 
         incisob (a^.hi, cantimpares);
+        incisoGG(a^.dato.finales, cantimpares);
         incisob (a^.hd, cantimpares); 
     end; 
 end; 
 
 var
-    l: lista;
     a: arbol;
     cantimpares: integer; 
 begin
-    l:= nil;
-    cantimpares:= 0; 
-    cargarlista(l);
-    cargararbol(a);
+    randomize;
+    a:= nil;
+	cantimpares:= 0; 
+	cargar (a);
     incisob (a, cantimpares); 
-    writeln (' la cantidad de alumnos con legajo impar es de: ', cantimpares); 
-end. {FALTA COMPLETAR!!!}
+    writeln (' la cantidad de alumnos con legajo impar es de: ', cantimpares);
+end.
