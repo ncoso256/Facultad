04f7ff3efd63dd448363afe93933896b2e7622cf
Noé <121284724+ncoso256@users.noreply.github.com>
2025-09-14 14:17:39 -0300

    Create ej2.pas

---
+++ Taller de Programacion/Imperativo/Practica adicionales/ej2.pas
@@ -0,0 +1,28 @@
+{
+2. Una agencia dedicada a la venta de autos ha organizado su stock y, tiene la información de
+los autos en venta. Implementar un programa que:
+
+a) Genere la información de los autos (patente, año de fabricación (2015..2024), marca,
+color y modelo, finalizando con marca ‘MMM’) y los almacene en dos estructuras de
+datos:
+
+i. Una estructura eficiente para la búsqueda por patente.
+
+ii. Una estructura eficiente para la búsqueda por marca. Para cada marca se deben
+almacenar juntas las patentes y colores de los autos pertenecientes a ella.
+
+b) Invoque a un módulo que reciba la estructura generado en a) i y una marca y retorne la
+cantidad de autos de dicha marca que posee la agencia.
+
+c) Invoque a un módulo que reciba la estructura generado en a) ii y una marca y retorne
+la cantidad de autos de dicha marca que posee la agencia.
+
+d) Invoque a un módulo que reciba el árbol generado en a) i y retorne una estructura con
+la información de los autos agrupados por año de fabricación.
+
+e) Invoque a un módulo que reciba el árbol generado en a) i y una patente y devuelva el
+modelo del auto con dicha patente.
+
+f) Invoque a un módulo que reciba el árbol generado en a) ii y una patente y devuelva el
+color del auto con dicha patente.
+}
