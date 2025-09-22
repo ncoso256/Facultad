d0bee79b044490624120a7fe70de99b0c7635bfc
Noé <121284724+ncoso256@users.noreply.github.com>
2025-09-22 15:40:04 -0300

    Create ej4.java

---
+++ Taller de Programacion/Objetos/Practica 1/ej4.java
@@ -0,0 +1,61 @@
+/*
+
+4- Un edificio de oficinas está conformado por 8 pisos (1..8) y 4 oficinas por piso
+(1..4). 
+Realice un programa que permita informar la cantidad de personas que
+concurrieron a cada oficina de cada piso. 
+Para esto, simule la llegada de personas al edificio de la siguiente manera: a cada persona se le pide el nro. de piso y nro. de
+oficina a la cual quiere concurrir. 
+La llegada de personas finaliza al indicar un nro. de piso 9. 
+Al finalizar la llegada de personas, informe lo pedido.
+
+*/
+package ejercicio4;
+
+/**
+ *
+ * @author noe
+ */
+import PaqueteLectura.Lector;
+public class Ejercicio4 {
+
+
+    public static void main(String[] args) {
+        int[][] oficinas = new int[8][4]; // declaro mi matriz
+        int piso; 
+        int nrooficina;
+        int i,j; // indices matriz
+        
+        // leo el piso y el nro de oficina
+       
+        System.out.println("Ingrese el numero de piso correspondiente(cuando quiera dejar de leer aprete el 9): ");
+        piso = Lector.leerInt(); 
+        System.out.println("Ingrese el numero de oficina correspondiente: ");
+        nrooficina = Lector.leerInt();
+        
+        // con dos for preguntando si es distinto de 9 recorro la matriz y filtro entre mi variable piso y nro oficina
+        
+        for (i = 0; piso != 9; i++){
+            for (j = 0; piso != 9; j++){
+                if (piso >= 1 && piso <= 8){  // las dobles condiciones en java se hacen en una sola condicion.. no como en pascal 
+                    if (nrooficina >= 1 && nrooficina <=4)
+                        oficinas[piso][nrooficina] += 1; // incremento la cantidad de personas filtrando la informacion por piso y oficina 
+                }
+
+                System.out.println("Ingrese el nro de piso correspondiente(cuando quiera dejar de leer aprete el 9): ");
+                piso = Lector.leerInt();   
+                System.out.println("Ingrese el numero de oficina correspondiente: ");
+                nrooficina = Lector.leerInt(); 
+                
+                // vuelvo a leer la informacion para seguir iterando
+            }
+        }
+        
+        for (i = 0; i < 8; i++){ // imprimo la informacion del edificio...
+            System.out.println("---------------------------");
+            for (j = 0; j < 4; j++)
+                System.out.print(" ( " + i + " , " + j + " ) " + oficinas[i][j] + " | ");
+        }
+    }
+    
+}
