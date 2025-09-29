df052590c1076a5f820419a56e87fb1e029d3456
Noé <121284724+ncoso256@users.noreply.github.com>
2025-09-29 14:29:39 -0300

    Update ej5.java

--- Taller de Programacion/Objetos/Practica 1/ej5.java
+++ Taller de Programacion/Objetos/Practica 1/ej5.java
@@ -27,28 +27,36 @@ public class Ejercicio5 {
         int i,j; // indices 
         double[][] calificacionesxaspecto = new double[clientes][aspectos]; // nada mas pongo en double las calificaciones para calcular el promedio
         
-        double suma = 0 , promedio = 0; // variables para el promedio
+        double suma, promedio = 0; // variables para el promedio
         
-        System.out.println("Lea la calificacion del cliente: ");
-        calificacion = Lector.leerInt(); // leo una calificacion
+       
         for (i = 0; i < 5; i++){  // recorro la matriz
             for (j = 0; j < 4; j++){ 
+                System.out.println("Lea la calificacion del cliente: ");
+                calificacion = Lector.leerInt(); // leo una calificacion
                 if (calificacion >= 1 && calificacion <= 10){ // leo calificaciones solo entre el 1 y 10
                     calificacionesxaspecto[i][j] = calificacion; // pongo la calificacion a la matriz
-                    suma += calificacionesxaspecto[i][j]; // sumo para el promedio
                 }
-                System.out.println("Lea la calificacion del cliente: ");
-                calificacion = Lector.leerInt(); // leo hasta que termine la ejecucion del for 
             }
         }
+        //suma += calificacionesxaspecto[i][j]; // sumo para el promedio
+        
+        
         for (i = 0; i < 5; i++){ // imprimo la matriz
             System.out.println("--------------------------");
             for (j= 0; j < 4; j++)
                 System.out.print(" ( " + i + " , " + j + " ) " + calificacionesxaspecto[i][j] + " | ");
         } 
-        promedio = suma / (clientes * aspectos);  // hago el promedio clientes*aspectos = 20 
-        System.out.println("La suma de mi matriz es de: " + suma);  // imprimo suma  
-        System.out.println("El promedio de mi matriz es de: " + promedio); // imprimo promedio
+        
+        for (j = 0; j < 4; j++){
+            suma = 0;
+            for (i = 0; i < 5 ; i++)
+                suma += calificacionesxaspecto[i][j];
+            promedio = suma / clientes ;  // hago el promedio   
+            System.out.println("La suma de mi matriz es de: " + suma);  // imprimo suma  
+            System.out.println("El promedio de mi matriz es de: " + promedio); // imprimo promedio
+        }
+
     }
     
 }
