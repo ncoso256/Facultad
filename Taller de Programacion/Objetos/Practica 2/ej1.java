db76f73984ccfd188d9435217d64d618bdc068ac
Noé <121284724+ncoso256@users.noreply.github.com>
2025-09-29 15:54:29 -0300

    Update ej1.java

--- Taller de Programacion/Objetos/Practica 2/ej1.java
+++ Taller de Programacion/Objetos/Practica 2/ej1.java
@@ -1,7 +1,17 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+1- Se dispone de la clase Persona (en la carpeta tema2). Un objeto persona puede crearse
+sin valores iniciales o enviando en el mensaje de creación el nombre, DNI y edad (en ese
+orden). Un objeto persona responde a los siguientes mensajes:
+getNombre() retorna el nombre (String) de la persona
+getDNI() retorna el dni (int) de la persona
+getEdad() retorna la edad (int) de la persona
+setNombre(X) modifica el nombre de la persona al “String” pasado por parámetro (X)
+setDNI(X) modifica el DNI de la persona al “int” pasado por parámetro (X)
+setEdad(X) modifica la edad de la persona al “int” pasado por parámetro (X)
+toString() retorna un String que representa al objeto. Ej: “Mi nombre es Mauro, mi
+DNI es 11203737 y tengo 70 años”
+Realice un programa que cree un objeto persona con datos leídos desde teclado. Luego
+muestre en consola la representación de ese objeto en formato String.
  */
 package tema2;
 
@@ -41,13 +51,12 @@ public class ejercicio1 {
         
         System.out.println(p.getNombre());
         System.out.println(p.getDNI());
-        System.out.println(p.getNombre());
+        System.out.println(p.getEdad()); // obtengo mi nombre, dni y edad.
         
-        System.out.println(p.toString());
+        System.out.println(p.toString()); // imprimo el objeto persona. 
         
         
         
     }
     
 }
-
