b08c9e85c3ab4bb64b91b5597ea9c203ac6213b1
Noé <121284724+ncoso256@users.noreply.github.com>
2025-10-01 11:54:46 -0300

    Update ej5.java

--- Taller de Programacion/Objetos/Practica 2/ej5.java
+++ Taller de Programacion/Objetos/Practica 2/ej5.java
@@ -1,1 +1,87 @@
-x
+/*
+
+5- Se dispone de la clase Partido (en la carpeta tema2). 
+Un objeto partido representa un encuentro entre dos equipos (local y visitante).
+Un objeto partido puede crearse sin valores iniciales o enviando en el mensaje de creación el nombre del equipo local, el
+nombre del visitante, la cantidad de goles del local y del visitante (en ese orden).
+
+Un objeto partido sabe responder a los siguientes mensajes:
+
+getLocal() retorna el nombre (String) del equipo local
+
+getVisitante() retorna el nombre (String) del equipo visitante
+
+getGolesLocal() retorna la cantidad de goles (int) del equipo local
+
+getGolesVisitante() retorna la cantidad de goles (int) del equipo visitante
+
+setLocal(X) modifica el nombre del equipo local al “String” X
+
+setVisitante(X) modifica el nombre del equipo visitante al “String” X
+
+setGolesLocal(X) modifica la cantidad de goles del equipo local al “int” X
+
+setGolesVisitante(X) modifica la cantidad de goles del equipo visitante al “int” X
+
+hayGanador() retorna un boolean que indica si hubo (true) o no hubo (false) ganador
+
+getGanador() retorna el nombre (String) del ganador del partido (si no hubo retorna
+un String vacío).
+
+hayEmpate() retorna un boolean que indica si hubo (true) o no hubo (false) empate
+
+Implemente un programa que cargue un vector con a lo sumo 20 partidos disputados en
+el campeonato. La información de cada partido se lee desde teclado hasta ingresar uno con
+nombre de visitante “ZZZ” o alcanzar los 20 partidos. 
+
+Luego de la carga:
+
+- Para cada partido, armar e informar una representación String del estilo:
+{EQUIPO-LOCAL golesLocal VS EQUIPO-VISITANTE golesVisitante }
+
+- Calcular e informar la cantidad de partidos que ganó River.
+
+- Calcular e informar el total de goles que realizó Boca jugando de local.
+
+ */
+package tema2;
+
+/**
+ *
+ * @author noe
+ */
+import PaqueteLectura.*;
+public class NewMain {
+
+
+    public static void main(String[] args) {
+        Partido[] partidos = new Partido[20];
+        String local,visitante;
+        int golesloc, golesvis;
+        int diml = 0;
+        String estilo;
+        
+        visitante = Lector.leerString(); 
+        while (diml < 20 && !visitante.equals("ZZZ")){
+            local = Lector.leerString();
+            golesloc = Lector.leerInt();
+            golesvis = Lector.leerInt();
+            partidos[diml] = new Partido(local,visitante,golesloc,golesvis);
+            diml++;    
+            visitante = Lector.leerString();
+        }
+        
+        int i, cant = 0 , total= 0;
+        for (i = 0; i < diml; i++){
+            estilo = (partidos[i].getLocal() + "  " + partidos[i].getGolesLocal() + " VS " + partidos[i].getVisitante() + "  " + partidos[i].getGolesVisitante());
+            System.out.println(estilo);
+            if (partidos[i].getLocal().equals("River") && partidos[i].getGanador().equals("River") && !partidos[i].hayEmpate() || partidos[i].getVisitante().equals("River")&& partidos[i].getGanador().equals("River") && !partidos[i].hayEmpate())
+                    cant++;
+            if (partidos[i].getLocal().equals("Boca")&& !partidos[i].getVisitante().equals("Boca"))
+                  total += partidos[i].getGolesLocal();  
+        }
+        System.out.println(cant);
+        System.out.println(total);
+    }
+    
+}
