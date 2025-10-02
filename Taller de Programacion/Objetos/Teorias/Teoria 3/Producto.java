b34f5d02c01f10e605c5cdd4ac7383a31ea9522b
Noé <121284724+ncoso256@users.noreply.github.com>
2025-10-02 20:59:04 -0300

    Update Producto.java

--- Taller de Programacion/Objetos/Teorias/Teoria 3/Producto.java
+++ Taller de Programacion/Objetos/Teorias/Teoria 3/Producto.java
@@ -1,48 +1,45 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package videostaller3;
-
-/**
- *
- * @author noe
- */
-public class Producto {
-    private double precio; // azul tipo primitivo 
-    private String nombre; // esta es un objeto
-    private String descripcion; // y esta variable son objetos
+package video3;
+        /*
+    Definicion de clases
     
-    public String nombre1; // esta mal las variables de instacia son privadas. 
-    
-    private double precio1 = 100; // le puedo dar un valor por defecto antes de inicializarlo en el ppal
-    private String nombre2 = "Pepe";
-    private String descripcion1 = "Sin desc";
-    
-    private double precio2 = 10; 
-    /*
-    
-    public TipoRetorno nombreMetodo(lista de parametros formales){
-        declaracion de variables locales al metodo
-        cuerpo del metodo
+    public class NombreDeClase(){
+        
+        // declaracion del estado del objeto
+        
+        // declaracion de constructores
+        
+        // declaracion de metodos que implementan acciones
     }
     
-    public puede ser private tambien 
-    
-    tipo de retorno: dato primitivo, nombre de clase o void no retorna datos
-    
-    lista de parametros: siempre por valor se separa con coma ;   tipoprimitivo nombreParam , NombreClase nombreParam 
-    
-    cuerpo: codigo y devuelve resultado return 
     */
+
+public class Producto {
+    private double precio = 10; // primitiva
+    private String nombre = "Pepe"; // objeto
+    private String descripcion = "Sin descripcion" ; // private para encapsulamiento
     
-    public boolean esCaro(double valor){ 
-        return (precio2 > valor); 
+
+    /*
+    Declaracion del comportamiento = 
+    
+    public tipoRetorno nombreMetodo(Lista de parametros formales){
+        // declaracion de variables locales al metodo
+        // cuerpo del metodo
     }
-    private boolean esCaro2(double valor2){
-        return (precio1 > valor2);
-    }
+    
+    public = forma parte de la interfaz
+    
+    tipoRetorno = primitivos / nombre de clase / void (no retorna dato)
+    
+    Lista de parametros = datos primitivos o objetos 
+                        = tipoPrimitivo // NombreClase nombreParam
+                        = se separa con coma 
+                        = pasaje por valor unicamente
+    
+    Declaracion de variables locales = Duran solo en el metodo
+    
+    Cuerpo = Devolver resultado return
+    */
     
     public double getPrecio(){
         return precio;
@@ -51,7 +48,6 @@ public class Producto {
     public void setPrecio(double nuevoPrecio){
         precio = nuevoPrecio;
     }
-
     public String getNombre() {
         return nombre;
     }
@@ -67,51 +63,20 @@ public class Producto {
     public void setDescripcion(String descripcion) {
         this.descripcion = descripcion;
     }
-
-    public String getNombre1() {
-        return nombre1;
-    }
-
-    public void setNombre1(String nombre1) {
-        this.nombre1 = nombre1;
-    }
-
-    public double getPrecio1() {
-        return precio1;
-    }
-
-    public void setPrecio1(double precio1) {
-        this.precio1 = precio1;
-    }
-
-    public String getNombre2() {
-        return nombre2;
-    }
-
-    public void setNombre2(String nombre2) {
-        this.nombre2 = nombre2;
-    }
-
-    public String getDescripcion1() {
-        return descripcion1;
-    }
-
-    public void setDescripcion1(String descripcion1) {
-        this.descripcion1 = descripcion1;
-    }
-
-    public double getPrecio2() {
-        return precio2;
-    }
-
-    public void setPrecio2(double precio2) {
-        this.precio2 = precio2;
+    
+    
+    public boolean esCaro(double valor){
+        return (precio > valor);
     }
 
     @Override
-    public String toString() { // armado por insert code tiene que ser si o si toString el nombre porque si no es no puedo hacer
-        // system.out.println(p2); p2 = producto y hace el casteo del toString
-        return "Producto{" + "precio=" + precio + ", nombre=" + nombre + ", descripcion=" + descripcion + ", nombre1=" + nombre1 + ", precio1=" + precio1 + ", nombre2=" + nombre2 + ", descripcion1=" + descripcion1 + ", precio2=" + precio2 + '}';
+    public String toString() {
+       String aux = "Producto:" + nombre + " vale " + precio;
+       aux += esCaro(500); 
+       return aux;
     }
+
+    
     
 }
+
