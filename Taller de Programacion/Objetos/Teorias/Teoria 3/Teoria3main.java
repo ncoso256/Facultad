430b8ebf956591b0b22a37c65d04a9baac0de54b
Noé <121284724+ncoso256@users.noreply.github.com>
2025-10-02 20:59:31 -0300

    Update Teoria3main.java

--- Taller de Programacion/Objetos/Teorias/Teoria 3/Teoria3main.java
+++ Taller de Programacion/Objetos/Teorias/Teoria 3/Teoria3main.java
@@ -1,53 +1,30 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package videostaller3;
+package video3;
 
-/**
- *
- * @author noe
- */
-public class VideosTaller3 { // el nombre de la clase coincide con el archivo.java
+public class VideosTaller3 {
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
-        // TODO code application logic here
-        Producto p = new Producto();
-        System.out.println(p.toString());
-        
-        if (p.esCaro(50)) // con precio2 no me paso 
-            System.out.println("Te pasaste de precio");
-        
-        if (p.esCaro2(50)) // esCaro2 tiene acceso privado solo se pueden ejecutar metodos publicos
-            System.out.println("Te pasaste de precio");
-        
+       Producto p = new Producto();
         Producto p2 = new Producto();
-        if (p2.esCaro(20))
-            System.out.println("Te pasaste de precio");
         
-        Libro l1 = new Libro();
-        int x = 1;
-        l1.hacerUno(x);
-        System.out.println(x); // imprime 1 porque es una copia x
+        Producto[] productos = new Producto[2];
+        productos[0] = new Producto(); 
+        productos[0].setNombre("El de la pos 0");
+        productos[1].getPrecio();
         
-        Libro l2 = new Libro();
-        l2.setTitulo("Java");
-        l1.hacerDos(l2);
-        System.out.println(l2.getTitulo()); // imprime otro el parametro cambio
+        // l^.sig := nil; error 
         
-        l2.setTitulo("Java");
-        l1.hacerTres(l2);
-        System.out.println(l2.getTitulo()); // imprime java porque hacer tres hace un new de una variable de instancia en libro
-        
+        p2.setNombre("Titulo de p: ");
         p2.setPrecio(200);
-        if (p.esCaro(50))  
-            System.out.println("Te pasaste de precio: " + p.getPrecio());
+        if (p.esCaro(5))
+            System.out.println("Te pasaste de precio: " +  p.getPrecio());
+        
         if (p2.esCaro(20))
-            System.out.println("Te pasaste de precio: " + p2.getPrecio());
+            System.out.println("Te pasaste de precio: "+ p2.getPrecio());
+        
+        p2.setNombre("Titulo de prueba de p2: ");
+        System.out.println(p2);
     }
     
 }
+
