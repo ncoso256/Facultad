669ed989b966eb8c8b92d22c0d66557fe6cf3a09
Noé <121284724+ncoso256@users.noreply.github.com>
2025-10-06 11:59:26 -0300

    Create Cliente.java

---
+++ Taller de Programacion/Objetos/Practica 3/ej4/Cliente.java
@@ -0,0 +1,49 @@
+package tema3;
+
+
+public class Cliente {
+    private String nombre;
+    private int DNI;
+    private int edad; 
+    
+    public Cliente(String unNombre, int unDNI, int unaEdad){
+        nombre = unNombre;
+        DNI = unDNI;
+        edad = unaEdad; 
+    }
+    
+    public Cliente(){
+     
+    }
+
+    public int getDNI() {
+        return DNI;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setDNI(int unDNI) {
+        DNI = unDNI;
+    }
+
+    public void setEdad(int unaEdad) {
+        edad = unaEdad;
+    }
+
+    public void setNombre(String unNombre) {
+        nombre = unNombre;
+    }
+    
+    public String toString(){
+        String aux; 
+        aux = "El nombre del cliente es " + nombre + ", su DNI es " + DNI + " y tiene " + edad + " años.";
+        return aux;
+    }
+    
+}
