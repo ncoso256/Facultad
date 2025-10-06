0ad4d7dbc3f2d02d26595f34aef0db3763cbe6b9
Noé <121284724+ncoso256@users.noreply.github.com>
2025-10-06 12:34:07 -0300

    Create Circulo.java

---
+++ Taller de Programacion/Objetos/Practica 3/ej5/Circulo.java
@@ -0,0 +1,54 @@
+
+package tema3;
+
+/**
+ *
+ * @author noe
+ */
+public class Circulo {
+    private double radio;
+    private String colorRelleno;
+    private String colorLinea;
+
+    public Circulo(double radio, String colorRelleno, String colorLinea) {
+        this.radio = radio;
+        this.colorRelleno = colorRelleno;
+        this.colorLinea = colorLinea;
+    }
+
+    public double getRadio() {
+        return radio;
+    }
+
+    public void setRadio(double radio) {
+        this.radio = radio;
+    }
+
+    public String getColorRelleno() {
+        return colorRelleno;
+    }
+
+    public void setColorRelleno(String colorRelleno) {
+        this.colorRelleno = colorRelleno;
+    }
+
+    public String getColorLinea() {
+        return colorLinea;
+    }
+
+    public void setColorLinea(String colorLinea) {
+        this.colorLinea = colorLinea;
+    }
+    
+    public double calcularPerimetro(){
+        double perimetro;
+        perimetro = 2 * Math.PI * this.radio;
+        return perimetro;
+    }
+    
+    public double calcularArea(){
+        double area;
+        area = Math.PI * (this.radio *this.radio);
+        return area;
+    }
+}
