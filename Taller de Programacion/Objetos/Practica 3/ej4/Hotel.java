30760a3b23cf7cf5c2113163d2f28ccdcb1c5564
Noé <121284724+ncoso256@users.noreply.github.com>
2025-10-06 11:58:44 -0300

    Create Hotel.java

---
+++ Taller de Programacion/Objetos/Practica 3/ej4/Hotel.java
@@ -0,0 +1,49 @@
+package tema3;
+
+/**
+ *
+ * @author noe
+ */
+public class Hotel {
+    private int N; 
+    private Cliente[] clientes = null;
+    private Habitacion hab; 
+
+    
+    public Hotel(int N) {
+        this.N = N;
+    }
+    
+    public Cliente[] ingresarCliente(Cliente c,boolean ocupa, int x){
+        if (clientes[x] == null && !ocupa)
+            clientes[x] = c;
+        return clientes;
+    }
+    
+    
+    public int getN() {
+        return N;
+    }
+
+    public void setN(int N) {
+        this.N = N;
+    }
+
+    public Cliente[] getHabitaciones() {
+        return clientes;
+    }
+
+    public void setHabitaciones(Cliente[] clientes) {
+        this.clientes = clientes;
+    }
+
+    @Override
+    public String toString() {
+        int i = 0;
+        String aux;
+        aux = "Habitación: " + i +  "  " + "costo: " + hab.getCostopornoche() + "  " + " libre u ocupada: " + hab.isestaOkupa();
+        if (clientes != null)
+            aux += "info del cliente: " + this.clientes;
+        return aux;
+    }
+}
