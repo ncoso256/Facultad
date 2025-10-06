65fa3039b18b304eb6b99a52445b3b01ff14be74
Noé <121284724+ncoso256@users.noreply.github.com>
2025-10-06 15:32:04 -0300

    Create ProductoQueCaduca.java

---
+++ Taller de Programacion/Objetos/Teorias/Teoria4/Teoria 4 Parte 1/ProductoQueCaduca.java
@@ -0,0 +1,20 @@
+
+package videostaller4;
+
+/**
+ *
+ * @author noe
+ */
+public class ProductoQueCaduca extends Producto{
+    private int meses;
+
+    public int getMeses() {
+        return meses;
+    }
+
+    public void setMeses(int meses) {
+        this.meses = meses;
+    }
+    
+    
+}
