4dcce60f3feb54317d3103a7e73c828f39dcc632
Noé <121284724+ncoso256@users.noreply.github.com>
2025-10-09 12:11:16 -0300

    Create main2.java

---
+++ Taller de Programacion/Objetos/Practica 4/ej2/main2.java
@@ -0,0 +1,24 @@
+
+package tema4;
+
+import PaqueteLectura.Lector;
+
+/**
+ *
+ * @author noe
+ */
+public class main2 {
+
+    public static void main(String[] args) {
+
+        
+        Jugador j = new Jugador(10,11,"facu",50,4);
+        
+        Entrenador e = new Entrenador("fdjklfdlj",54,4,7);
+        
+        System.out.println(j.toString());
+        
+        System.out.println(e.toString());
+    }
+    
+}
