6b11364fa771bfb4be8029dc3dbe63bfd7a1c502
Noé <121284724+ncoso256@users.noreply.github.com>
2025-10-09 10:39:34 -0300

    Create Empleado.java

---
+++ Taller de Programacion/Objetos/Practica 4/ej2/Empleado.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tema4;
+
+/**
+ *
+ * @author Usuario
+ */
+public abstract class Empleado {
+    private String nombre;
+    private double sueldo;
+    private int antiguedad;
+    
+    public Empleado(String unNombre, double unSueldo, int unaAntiguedad){
+        setNombre(unNombre);
+        setSueldo(unSueldo);
+        setAntiguedad(unaAntiguedad);
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getSueldo() {
+        return sueldo;
+    }
+
+    public void setSueldo(double sueldo) {
+        this.sueldo = sueldo;
+    }
+
+    public int getAntiguedad() {
+        return antiguedad;
+    }
+
+    public void setAntiguedad(int antiguedad) {
+        this.antiguedad = antiguedad;
+    }
+    
+    public abstract double calcularEfectividad();
+    
+}
