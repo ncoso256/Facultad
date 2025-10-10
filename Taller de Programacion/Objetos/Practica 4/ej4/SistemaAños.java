6b1284f0d7e2276818c39c43e93db6d82b8b9b47
Noé <121284724+ncoso256@users.noreply.github.com>
2025-10-10 19:48:43 -0300

    Create SistemaAños.java

---
+++ Taller de Programacion/Objetos/Practica 4/ej4/SistemaAños.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tema4;
+
+/**
+ *
+ * @author Usuario
+ */
+public class SistemaAños extends Sistema{
+    public SistemaAños(Estacion esta, int A,int N){
+        super(esta,A,N);
+    }
+    
+    public void toString2(){
+        double[][] t = super.getTemperatura();
+        int temp;
+        
+        System.out.println(super.getEsta().getNom()+ " " + super.getEsta().getLatitud() + " " + super.getEsta().getLongitud());
+        
+        int i,j;
+        for (i = 0; i < t.length; i++){
+            double total = 0;
+            for (j = 0; j < 12 ; j++){
+                if (t[i][j] != 99999)
+                    total += t[i][j];
+            }
+            temp = super.getAno()+ i;
+            System.out.println("año " + temp + " temperatura " + total/ 12 + "\n");
+        }
+    }
+}
