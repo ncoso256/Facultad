14d59f40f0df55add86fa8ad5f4fbde700a581f5
Noé <121284724+ncoso256@users.noreply.github.com>
2025-10-12 18:11:54 -0300

    Update Main5.java

--- Taller de Programacion/Objetos/Practica 4/ej5/Main5.java
+++ Taller de Programacion/Objetos/Practica 4/ej5/Main5.java
@@ -1,1 +1,22 @@
 
+package tema4;
+
+
+public class Main5 {
+
+    
+    public static void main(String[] args) {
+        Dibujo d = new Dibujo("un dibujo");
+        
+        Cuadrado c1 = new Cuadrado(10,"violeta","Rosa");
+        Rectangulo r = new Rectangulo(20,10,"Azul","Celeste");
+        Cuadrado c2 = new Cuadrado(30,"Rojo","Naranja");
+        
+        d.agregar(c1);
+        d.agregar(r);
+        d.agregar(c2);
+        
+        d.mostrar();
+    }
+    
+}
