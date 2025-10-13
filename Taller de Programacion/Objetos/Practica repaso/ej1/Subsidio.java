92077664cc796357e0d7f42bc872e468ffd99e8e
Noé <121284724+ncoso256@users.noreply.github.com>
2025-10-13 17:32:17 -0300

    Create Subsidio.java

---
+++ Taller de Programacion/Objetos/Practica repaso/ej1/Subsidio.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practicarepaso1;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Subsidio {
+    private double monto;
+    private String motivo;
+    private boolean otorgado;
+    
+    public Subsidio(double unMonto, String unMotivo){
+        this.monto = unMonto;
+        this.motivo = unMotivo;
+        this.otorgado = false;
+    }
+
+    public double getMonto() {
+        return monto;
+    }
+
+    public boolean isOtorgado() {
+        return otorgado;
+    }
+
+    public void setOtorgado(boolean otorgado) {
+        this.otorgado = otorgado;
+    }
+    
+}
