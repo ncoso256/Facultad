2612c6bd03893d3812000efcb511c71108d62dbb
Noé <121284724+ncoso256@users.noreply.github.com>
2025-10-15 14:52:27 -0300

    Create CursoBasico.java

---
+++ Taller de Programacion/Objetos/Parciales/Parcial 1 /CursoBasico.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication7;
+
+/**
+ *
+ * @author noe
+ */
+public class CursoBasico extends Curso{
+
+    public CursoBasico(int M, String unNombre, int unCosto, String unComienzo) {
+        super(M, unNombre, unCosto, unComienzo);
+    }
+    
+    
+    
+    public Alumno mejorDesempeño(){
+        int max = -1;
+        int i;
+        Alumno alu = null;
+        for (i = 0; i < super.getM() ; i++){    
+            if (super.getAlumnos()[i].getCantTareas() > max){
+                max = super.getAlumnos()[i].getCantTareas();
+                alu = super.getAlumnos()[i];
+            }
+        }
+        return alu;
+    }
+
+
+    
+    
+}
